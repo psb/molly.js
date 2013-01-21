c68c6cc8eac2e1e11d364adeefc8555c2bd595a8
Paul Bacchus <paulsbacchus@gmail.com>
2013-01-20 16:40:50 -0800

    Created express server to serve JSON data from the DB.

---
+++ server.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var app = express();
+var r = require('rethinkdb');
+
+// Connect to DB
+var conn;
+r.connect(
+  { host:'localhost', port:28015 },
+  function( connection ) {
+    conn = connection;
+    // Select the correct DB
+    conn.use('pdb_compounds');
+  },
+  function( err ) {
+    throw "Connection Failed";
+  }
+);
+
+app.get('/:cifID', function(req, res){
+  var query = req.params.cifID;
+  // Query the DB
+  conn.run(
+    r.table( 'compounds').get(query, '_chem_comp.id' ),
+    function( data ) {
+      // result = data;
+      res.send( data ? data : '404' );
+    }
+  );
+
+});
+
+app.listen(9000);
