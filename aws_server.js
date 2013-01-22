7eb592359adebc2b3cdd55aa72dd19cf6fbb2318
Paul Bacchus <paulsbacchus@gmail.com>
2013-01-22 11:45:33 -0800

    Changed the aws server to use a more specific url.

---
+++ aws_server.js
@@ -0,0 +1,39 @@
+var express = require( 'express' );
+var r = require( 'rethinkdb' );
+var cors = require( './cors' );
+var _ = require( 'underscore' );
+// Create app and use cors
+var app = express();
+app.use(cors);
+
+// Connect to DB
+var conn;
+r.connect(
+  { host:'localhost', port:28015 },
+  function( connection ) {
+    conn = connection;
+    // Select the correct DB
+    conn.use('pdb_compounds');
+  },
+  function( err ) {
+    throw "Connection Failed";
+  }
+);
+
+app.get('/pdb_compounds/:cifIDs', function( req, res ){
+  var queries = req.params.cifIDs.split('&');
+  // Uppercase queries before querying the database
+  var upperQueries = _.map( queries, function( qry ){
+    return qry.toUpperCase();
+  });
+  // Query the DB
+  r.expr( upperQueries ).map(function( query ){
+    return r.table( 'compounds' ).get( query, '_chem_comp.id' );
+  }).run().collect(function( results ){
+    var data = _.object( queries, results );
+    res.send( data );
+  });
+});
+
+app.listen(80);
+console.log('express listeing at: %d', 80);
